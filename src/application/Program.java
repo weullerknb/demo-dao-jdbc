97fe0f1a54f157afb8ccfa7b2908a1ba72268721
Weuller Oliveira <weullerwww@gmail.com>
2022-08-03 14:33:23 -0300

    Classe DaoFactory e SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 
 		Department dp = new Department(1, "Books");
 		Seller seller = new Seller(1, "Sasuke", "sasuke@gmail.com", new Date(), 3000.0, dp);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 	}
